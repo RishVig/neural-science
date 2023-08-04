33aa445223fbfdb4f1bbf7db58503257b07e8593
RishVig <133171943+RishVig@users.noreply.github.com>
2023-08-04 19:16:36 +0800

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,11 +1,12 @@
 import numpy as np
+import nnfs 
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+nnfs.init()
 
-class Layer_Dense:
+X, y = spiral_data(100, 3)
+
+class Layer_dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
@@ -13,11 +14,23 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
-layer_1 = Layer_Dense(4,5)
-layer_2 = Layer_Dense(5,2)
+# Create the layers and perform the forward pass
+layer_1 = Layer_dense(2, 5)  # n_inputs = 2 (since we have 2 features), n_neurons = 5
+activation_1 = Activation_ReLU()
+
+layer_1 = Layer_dense(2,5)
 
 layer_1.forward(X)
+activation_1 = Activation_ReLU()
+layer_1.forward(X)
 
-layer_2.forward(layer_1.output)
-print(layer_2.output)
+activation_1.forward(layer_1.output)
+print(np.version)
+print(activation_1.output)
+
+# Now, the output of the first layer after applying ReLU activation is in activation_1.output
+print("Thank You for viewing our data!!")
